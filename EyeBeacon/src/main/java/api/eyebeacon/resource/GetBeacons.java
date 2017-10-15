e16ac3281c02f61d8f7043edd172c81df8b6a870
Erik Wolters <erik-wolters@outlook.com>
2017-10-15 12:37:22 +0200

    Changed API output to JSON

--- EyeBeacon/src/main/java/api/eyebeacon/resource/GetBeacons.java
+++ EyeBeacon/src/main/java/api/eyebeacon/resource/GetBeacons.java
@@ -1,11 +1,18 @@
 package api.eyebeacon.resource;
 
+import java.io.IOException;
 import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 /**
  * Retrive Beacon List
@@ -15,50 +22,51 @@ import javax.ws.rs.core.MediaType;
 @Path("beacons")
 public class GetBeacons {
 
+    private ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+    
+    
     @GET
-    @Produces(MediaType.TEXT_HTML)
-    public String returnBeaconList() {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response returnBeaconList() throws JSONException, IOException {
         BeaconResource beaconlist = new BeaconResource();
-
-        StringBuilder sb = new StringBuilder();
-
-        for (Beacon beacon : beaconlist.getBeacons()) {
-            sb.append("<ul>");
-            sb.append("<li>").append(beacon.getId()).append("</li>");
-            sb.append("<li>").append(beacon.getName()).append("</li>");
-            sb.append("<li>").append(beacon.getLatitude()).append("</li>");
-            sb.append("<li>").append(beacon.getLongtitude()).append("</li>");
-            sb.append("</ul><br />");
-        }
+        
+        String json = ow.writeValueAsString(beaconlist);
 
         beaconlist = null;
         Beacon.resetId();
 
-        return sb.toString();
+        return Response.ok(json, MediaType.APPLICATION_JSON).build();
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("/{beaconId}")
-    public String returnBreacon(@PathParam("beaconId") int id) {
+    public Response returnBreacon(@PathParam("beaconId") int id)
+            throws JSONException, IOException {
+
         BeaconResource beaconlist = new BeaconResource();
 
-        StringBuilder sb = new StringBuilder();
+        Beacon returnBeacon = null;
 
         for (Beacon beacon : beaconlist.getBeacons()) {
             if (beacon.getId() == id) {
-                sb.append("<ul>");
-                sb.append("<li>").append(beacon.getId()).append("</li>");
-                sb.append("<li>").append(beacon.getName()).append("</li>");
-                sb.append("<li>").append(beacon.getLatitude()).append("</li>");
-                sb.append("<li>").append(beacon.getLongtitude()).append("</li>");
-                sb.append("</ul><br />");
+                returnBeacon = beacon;
             }
         }
-        
+
+        if (returnBeacon == null) {
+            beaconlist = null;
+            Beacon.resetId();
+
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("No beacon found with id: " + id).build();
+        }
+
         beaconlist = null;
         Beacon.resetId();
         
-        return sb.toString();
+        String json = ow.writeValueAsString(returnBeacon);
+        return Response.ok(json, MediaType.APPLICATION_JSON).build();
+
     }
 }
