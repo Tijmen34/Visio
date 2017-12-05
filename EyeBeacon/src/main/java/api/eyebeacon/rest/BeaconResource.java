4c0585d2987503295d9e6adee672003cd4c79f04
Thijs Timmermans <thijs-97@hotmail.com>
2017-12-05 11:24:09 +0100

    create resource classes + added db methods

--- EyeBeacon/src/main/java/api/eyebeacon/rest/BeaconResource.java
+++ EyeBeacon/src/main/java/api/eyebeacon/rest/BeaconResource.java
@@ -2,38 +2,88 @@ package api.eyebeacon.rest;
 
 import api.eyebeacon.model.Beacon;
 import api.eyebeacon.model.DatabaseConn;
+import api.eyebeacon.rest.model.ClientError;
+import com.mongodb.client.FindIterable;
+import java.io.IOException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
+import org.json.JSONException;
 
 /**
- * Generate a hardcoded list of beacons for testing purposes
- * 
- * @author  
- * @deprecated Only used for testing
+ * adds beacons to db
+ *
+ * @author
+ *
  */
-//@Deprecated
 @Path("/beacons")
-
 public class BeaconResource {
-      DatabaseConn db = new DatabaseConn();
+
+    DatabaseConn db = new DatabaseConn();
     private final ObjectWriter OBJECT_WRITER = new ObjectMapper().writer()
             .withDefaultPrettyPrinter();
-    
-   
+
+    /**
+     * Get a list of al beacons
+     *
+     * @return JSON response with the beacons or an error.
+     * @throws JSONException
+     * @throws IOException
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBeaconList() throws JSONException, IOException {
+
+//      Load alist of beacons
+        DatabaseConn db = new DatabaseConn();
+        FindIterable beaconColl = db.findDocsInColl("beacon");
+
+        // Check if there are beacons in the list
+        if (db.findDocsInColl("beaconAmsterdam") == null) {
+            // Create the client error
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new ClientError("No beacons were found"))
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Methods",
+                            "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                    .build();
+        }
+
+//       Format the array to JSON
+        String json = OBJECT_WRITER.writeValueAsString(beaconColl);
+
+        return Response.ok(json, MediaType.APPLICATION_JSON)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods",
+                        "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .build();
+
+    }
+
     /**
      * Generate testing beacons
      */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Beacon addBeacon(Beacon beacon){
-        System.out.println("beacon"+ beacon);
-        return db.addBeacon(beacon);  
-        
+    public Beacon addBeacon(Beacon beacon) {
+        System.out.println("beacon" + beacon);
+        return db.addBeacon(beacon);
+
     }
+    
+//  @DELETE
+//  @Path("{id}")
+//  public Beacon deleteBeacon(@PathParam("id") int id) {
+//      return db.deleteBeacon(id);
+//  }
+    
 }
