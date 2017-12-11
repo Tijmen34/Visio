531907b835dd0bfc1b369d2a69dd4cc9a17f0bf8
Unknown <feest55@hotmail.com>
2017-12-11 13:14:08 +0100

    Swagger almost working
    No visual yet

--- EyeBeacon/src/main/java/api/eyebeacon/model/User.java
+++ EyeBeacon/src/main/java/api/eyebeacon/model/User.java
@@ -9,13 +9,21 @@ public class User {
     private String name;
     private String email;
     private String password;
+    private String id;
+    
+    public User(){ }
 
-    public User(String name, String email, String password) {
+    public User(String name, String email, String password, String id) {
+        
         this.name = name;
         this.email = email;
         this.password = password;
+        this.id = id;
     }
-
+    public String getId(){
+      return id;
+    }
+    
     public String getName() {
         return name;
     }
